b16c73cd49f3858193b829728b9651a864c11577
julien <julien.pichonnat@gmail.com>
2018-01-05 11:50:41 +0100

    2.1.2 ajout de 2 variables long + affichage

--- Hello.java
+++ Hello.java
@@ -2,9 +2,20 @@ public class Hello
 {
 	public static void main (String [] args)
 	{
+
+		
+		long metre = 1;
+		long yard = 1;
+		
+		System.out.println("metre ="+metre+", yard ="+yard);
+		
+		
+		
+		
 	System.out.println("hello world");
-	
-	
-	
+	}
+	public static void addition (long gauche, long droite)
+	{
+	gauche = gauche + droite;
 	}
 }
