6320cdf55fd98ee4034ede064524d6909f20bb36
julien <julien.pichonnat@gmail.com>
2018-01-05 11:59:31 +0100

    2.1.3 utilisation de variable dans l'addition

--- Hello.java
+++ Hello.java
@@ -1,21 +1,28 @@
 public class Hello
 {
+		public static void addition (long gauche, long droite)
+		{
+		gauche = gauche + droite;
+		}
+		
 	public static void main (String [] args)
 	{
 
 		
+		
+		
 		long metre = 1;
 		long yard = 1;
 		
+		
+		System.out.println("hello world");
+		System.out.println("metre ="+metre+", yard ="+yard);
+		
+		addition (metre,yard);
 		System.out.println("metre ="+metre+", yard ="+yard);
 		
 		
-		
-		
-	System.out.println("hello world");
+
 	}
-	public static void addition (long gauche, long droite)
-	{
-	gauche = gauche + droite;
-	}
+
 }
